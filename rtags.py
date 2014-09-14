32e0694cd978e620c317471379efc1afe173243c
Sergei Turukin <rampage644@gmail.com>
2014-09-14 15:36:10 +0300

    Added location-based query

--- rtags.py
+++ rtags.py
@@ -6,27 +6,39 @@ import re
 BIN = '/home/ramp/mnt/git/rtags/build/bin/rc'
 # pth_sleep
 reg = r'(\S+):(\d+):(\d+):(.*)'
-class RtagsCommand(sublime_plugin.TextCommand):
-  def run(self, edit, switch):
+class RtagsBaseCommand(sublime_plugin.TextCommand):
+  def run(self, edit, switch, *args, **kwargs):
     p = subprocess.Popen([BIN,
                          switch, 
-                         self.view.substr(self.view.word(self.view.sel()[0])), 
+                         self._query(*args, **kwargs), 
                          '--silent-query'],
      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     items = list(map(lambda x: x.decode('utf-8'), out.splitlines()))
     self.last_references = items
     def out_to_items(item):
-      (file, _, _, usage) = re.findall(reg, item)[0]
-      return [file.split('/')[-1], usage.strip()]
+      (file, line, _, usage) = re.findall(reg, item)[0]
+      return [usage.strip(), "{}:{}".format(file.split('/')[-1], line)]
     items = list(map(out_to_items, items))
     self.view.window().show_quick_panel(items, self.on_select)
 
   def on_select(self, res):
+    if res == -1:
+      return
     (file, line, col, _) = re.findall(reg, self.last_references[res])[0]
-    view = self.view.window().open_file('%s:%s:%s' % (file, line, col), sublime.ENCODED_POSITION)
-    self.view.window().focus_view(view)
-    view.sel().clear()
-    p = view.text_point(int(line)-1, 0)
-    print(p)
-    view.sel().add(sublime.Region(p))
+    view = self.view.window().open_file('%s:%s' % (file, line), sublime.ENCODED_POSITION)
+
+  def _query(self, *args, **kwargs):
+    return ''
+
+class RtagsSymbolNameCommand(RtagsBaseCommand):
+  def _query(self, *args, **kwargs):
+    return self.view.substr(self.view.word(self.view.sel()[0]))
+
+
+class RtagsLocationCommand(RtagsBaseCommand):
+  def _query(self, *args, **kwargs):
+    row, col = self.view.rowcol(self.view.sel()[0].a)
+    return '{}:{}:{}'.format(self.view.file_name(),
+                             row+1, col+1)
+    
